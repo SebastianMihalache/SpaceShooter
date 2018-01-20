78286817f9adf922f463b0b1a074abe112acd7d6
SebastianMihalache <34120620+SebastianMihalache@users.noreply.github.com>
2018-01-21 02:42:17 +0800

    setting the count score to the new UI text

--- Assets/Scripts/DestroyLazor.cs
+++ Assets/Scripts/DestroyLazor.cs
@@ -6,6 +6,20 @@ public class DestroyLazor : MonoBehaviour {
 
     public GameObject explosion;
     public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController gameController;
+
+    private void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null) {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameControllerObject == null)
+        {
+            Debug.Log ("Cannot fing 'GameController' script");
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -19,6 +33,7 @@ public class DestroyLazor : MonoBehaviour {
         {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
         }
+        gameController.AddScore(scoreValue);
         Destroy(other.gameObject);
             Destroy(gameObject);
         
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,22 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
     public GameObject hazard;
     public Vector3 spawnValues;
+    public int hazzardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public Text scoreText;
+    private int score;
 
     private void Start()
     {
-        SpawnWaves();
+       StartCoroutine (SpawnWaves());
     }
 
-    void SpawnWaves ()
+    IEnumerator SpawnWaves ()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range (-spawnValues.x, spawnValues.x),spawnValues.y,spawnValues.z);
-        Quaternion spawnRotation = Quaternion.identity;
-        Instantiate(hazard, spawnPosition, spawnRotation);
+        yield return new WaitForSeconds(startWait);
+        while (true) {
+            for (int i = 0; i < hazzardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(UnityEngine.Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+
+    }
+
+    private void UpdateScore ()
+    {
+        scoreText.text = "Count " + score.ToString ();
+
     }
 
 }
